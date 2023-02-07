fc7c8314176f1c9020a26abb6aa0f51b5f7993de
Joshua Hoblitt <josh@hoblitt.com>
2023-02-07 16:36:56 -0700

    import profile::nm::* from lsst-it/lsst-itconf repo

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,7 +9,61 @@ describe 'nm' do
         facts
       end
 
-      it { is_expected.to compile.with_all_deps }
+      context 'with no parameters' do
+        it { is_expected.to compile.with_all_deps }
+
+        it do
+          is_expected.to contain_file('/etc/sysconfig/network-scripts').with(
+            ensure: 'directory',
+            purge: true,
+            recurse: true,
+            force: true,
+          )
+        end
+
+        it do
+          is_expected.to contain_file('/etc/NetworkManager/conf.d').with(
+            ensure: 'directory',
+            purge: true,
+            recurse: true,
+            force: true,
+          )
+        end
+
+        it do
+          is_expected.to contain_file('/etc/NetworkManager/system-connections').with(
+            ensure: 'directory',
+            purge: true,
+            recurse: true,
+            force: true,
+          )
+        end
+
+        it do
+          is_expected.to contain_exec('nmcli conn reload').with(
+            command: '/bin/nmcli conn reload',
+            refreshonly: true,
+          )
+        end
+      end
+
+      context 'with connections param' do
+        let(:params) do
+          {
+            connections: {
+              foo: {
+                content: 'bar',
+              },
+              baz: {
+                content: 'quix',
+              },
+            },
+          }
+        end
+
+        it { is_expected.to contain_nm__connection('foo').with_content('bar') }
+        it { is_expected.to contain_nm__connection('baz').with_content('quix') }
+      end
     end
   end
 end
